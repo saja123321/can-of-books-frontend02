cd29f026607d21a0ce2d2a1a2907482002518a67
Saja Malkawi <sajamalkawi95@gmail.com>
2021-09-19 22:06:58 +0300

    front lab1 saja

--- src/Book.js
+++ src/Book.js
@@ -1,21 +1,33 @@
 import React, { Component } from "react";
 import Carousel from 'react-bootstrap/Carousel';
+import Badge from "react-bootstrap/Badge";
 
 
 class Book extends Component {
   render() {
     return (
-      <Carousel>
-        <Carousel.Item>
-          <Carousel.Caption>
-            <h2>{this.props.title}</h2>
-            <ul>
-              <li>{this.props.description}</li>
-              <li>{this.props.status}</li>
-                <li>{this.props.email}</li>
-            </ul>
-          </Carousel.Caption>
-        </Carousel.Item>
+      <Carousel style={{ hight: '200px' }}>
+        {this.props.books.map((b, index) => {
+          return (
+            <Carousel.Item interval={2000}>
+              <img
+                width='1000px'
+                height='500px'
+                style={{ objectFit: "contain" }}
+                className="d-block w-100"
+                src="https://wallpaperaccess.com/full/2890760.jpg"
+                alt="First slide"
+              />
+              <Carousel.Caption>
+
+                <Badge bg="success">{b.title}</Badge>
+                <p style={{ width: '70%', opacity: '0.7', display: 'block', margin: '50px auto', backgroundColor: '#FCF0C8', color: '#630A10' }} >{b.description}</p>
+                <Badge style={{ margin: '20px' }} bg="success">{b.status}</Badge>
+                <Badge bg="success">{b.email}</Badge>
+              </Carousel.Caption>
+            </Carousel.Item>
+          )
+        })}
       </Carousel>
     );
   }
--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import './login.css';
+// import './login.css';
 
 class Login extends React.Component {
   render() {
